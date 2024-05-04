bea362c7f4751e7e46a348a6a92650b803d3cd40
maxadmin00 <denis.samoshin00@gmail.com>
2024-05-05 01:03:06 +0300

    added api for predict

--- backend-service/app/main.py
+++ backend-service/app/main.py
@@ -2,16 +2,43 @@ from fastapi import FastAPI, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
 from random import random
 import time
+import app.services as services
+import torch
+import pandas as pd
+import numpy as np
+import joblib
 
 app = FastAPI()
 
-@app.get("/")
-async def get():
-    return "Welcome to starting page"
+'''@app.get("/")
+async def get_homepage():
+    return "Welcome to starting page"'''
 
-@app.websocket("/ws")
+'''@app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
         # data = await websocket.receive_text()
-        await websocket.send_text(f"{round(random(), 2)}")
+        await websocket.send_text(f"{round(random(), 2)}")'''
+
+@app.get("/predict/")
+async def get_predict():
+    scaler = joblib.load("app/data/scaler.joblib")
+    model = services.BTCModel()
+    model.load_state_dict(torch.load("app/data/TransformerBTC.pt", map_location=torch.device('cpu')))
+    model.eval()
+
+    df = pd.DataFrame(pd.read_csv("app/data/BTCUSDT_2020.csv").open.tail(100))
+    df.reset_index(drop=True, inplace=True)
+    data = scaler.transform(df)
+
+    for i in range(100):
+        tensor = services.create_dataset(data)
+        pred = services.predict(model, tensor)
+        data = np.append(data, np.array(pred))
+        data = data[1:]
+
+    preds = pd.DataFrame(data)
+    unscaled = scaler.inverse_transform(preds)
+    return unscaled.flatten().tolist()
+
---
+++ backend-service/app/services.py
@@ -0,0 +1,53 @@
+import sklearn
+import torch
+import torch.nn as nn
+import numpy as np
+import pandas as pd
+import torch.utils.data as data
+
+def create_dataset(dataset):
+    """Transform a time series into a prediction dataset
+
+    Args:
+        dataset: A numpy array of time series, first dimension is the time steps
+        lookback: Size of window for prediction
+    """
+    return torch.reshape(torch.Tensor(dataset), (1, 100, 1))
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout=0.2, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-np.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
+    
+class BTCModel(nn.Module):
+    def __init__(self, input_dim=1, d_model=256, nhead=4, num_layers=1, dropout=0.2):
+        super(BTCModel, self).__init__()
+
+        self.encoder = nn.Linear(input_dim, d_model)
+        self.pos_encoder = PositionalEncoding(d_model, dropout)
+        encoder_layers = nn.TransformerEncoderLayer(d_model, nhead)
+        self.transformer_encoder = nn.TransformerEncoder(encoder_layers, num_layers)
+        self.decoder = nn.Linear(d_model, 1)
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.pos_encoder(x)
+        x = self.transformer_encoder(x)
+        x = self.decoder(x[:, -1, :])
+        return x
+    
+def predict(model, X):
+    preds = model(X).flatten().tolist()
+    return preds
